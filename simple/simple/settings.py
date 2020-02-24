b32a6502da3a6351852be59d8b37dc17c0678014
Dan Encinales <cdencinales@gmail.com>
2020-02-23 23:26:05 -0800

    second commit (changed db engine to psql)

--- simple/simple/settings.py
+++ simple/simple/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'simple.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smefbuhq',
+        'USER': 'smefbuhq',
+        'PASSWORD': '60MjXAF3fo0actj2oCH5-bS4zsVQYk2f',
+        'HOST': 'otto.db.elephantsql.com',
+        'PORT': '5432',
+
     }
 }
 
